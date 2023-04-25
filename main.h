f8feaa51dc8eb0d40260c7bda3e1cc39c4bfcd2a
rajouuuuua <rajaarmili@gmail.com>
2023-04-25 15:31:27 -0700

    Task 1

--- main.h
+++ main.h
@@ -4,10 +4,28 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-/*FUNCTIONS*/
+/* Functions */
 
 int _puts(char *str);
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+/* Functions to print numbers */
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 #endif
