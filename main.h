4ab3e8887108f4fe2ff5cc3a37ad4d8437451d3d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-04-25 12:31:48 -0700

    header_file_print

--- main.h
+++ main.h
@@ -1,25 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
-#define BUFF_SIZE 1024
-
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
+#include <stddef.h>
 
 /*FUNCTIONS*/
 
 int _puts(char *str);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list v);
-int print_string(va_list args);
-int print_percent(va_list args);
 
 #endif
