a91e5d16b53bd6499b0eaaf09f168e20ca5df58b
rajouuuuua <rajaarmili@gmail.com>
2023-04-25 16:14:19 -0700

    Task1

--- main.h
+++ main.h
@@ -9,26 +9,11 @@
 int _puts(char *str);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
 
-
-/* Functions to print numbers */
-
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
+/* print functions.c module */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_s(va_list ap, params_t *params);
 #endif
